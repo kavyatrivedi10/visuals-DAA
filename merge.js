9d6b3115face420d7d8950eda0a9df3c19221c39
bhataktaBhai <naman.105.mishra@gmail.com>
2024-12-24 12:08:57 +0530

    made quickSort pretty

--- merge.js
+++ merge.js
@@ -94,10 +94,10 @@ async function merge(ele, low, mid, high){
 async function mergeSort(ele, l, r){
     console.log('In mergeSort()');
     if(l >= r){
-        console.log(`return cause just 1 elemment l=${l}, r=${r}`);
+        console.log(`return cause just 1 element l=${l}, r=${r}`);
         return;
     }
-    const m = l + Math.floor((r - l) / 2);
+    const m = Math.floor((l + r) / 2);
     console.log(`left=${l} mid=${m} right=${r}`, typeof(m));
     await mergeSort(ele, l, m);
     await mergeSort(ele, m + 1, r);
--- quick.js
+++ quick.js
@@ -1,17 +1,60 @@
+async function partitionRandom(ele, l, r){
+    console.log('In partitionRandom()');
+    // color pivot element
+    pivot = l + 1 + Math.floor(Math.random() * (r - l))
+    ele[pivot].style.background = 'red';
+    pivotHeight = ele[pivot].style.height;
+    console.log(pivotHeight);
+    indices = [...Array(r - l + 1).keys()].map(x => x + l)
+    console.log(indices)
+    left = indices.filter(j => j != pivot && parseInt(ele[j].style.height) <= parseInt(pivotHeight)).map(j => ele[j].style.height);
+    right = indices.filter(j => j != pivot && parseInt(ele[j].style.height) > parseInt(pivotHeight)).map(j => ele[j].style.height);
+    console.log(left);
+    let i = l;
+    for (const h of left) {
+        // ele[i].style.background = 'blue';
+        ele[i].style.background = 'yellow';
+        ele[i].style.height = h;
+        await waitforme(delay);
+        // ele[i].style.background = 'yellow';
+        i++;
+    }
+    ele[i].style.height = pivotHeight;
+    ele[i].style.background = 'green';
+    i++;
+    for (const h of right) {
+        // ele[i].style.background = 'blue';
+        ele[i].style.background = 'yellow';
+        ele[i].style.height = h;
+        await waitforme(delay);
+        // ele[i].style.background = 'yellow';
+        i++;
+    }
+    // pauseChamp
+    await waitforme(delay);
+    // color
+    for(let k = 0; k < ele.length; k++){
+        if(ele[k].style.background === 'yellow')
+            ele[k].style.background = 'cyan';
+    }
+
+    return l + left.length;
+}
 async function partitionLomuto(ele, l, r){
     console.log('In partitionLomuto()');
     let i = l - 1;
     // color pivot element
     ele[r].style.background = 'red';
     for(let j = l; j <= r - 1; j++){
-        console.log('In partitionLomuto for j');
+        console.log('In partitionLomuto for j = ' + j);
         // color current element
         ele[j].style.background = 'yellow';
         // pauseChamp
         await waitforme(delay);
 
         if(parseInt(ele[j].style.height) < parseInt(ele[r].style.height)){
-            console.log('In partitionLomuto for j if');
+            console.log('    In if partitionLomuto for j = ' + j);
+            console.log('i = ' + i + ';   j = ' + j);
             i++;
             swap(ele[i], ele[j]);
             // color 
@@ -30,13 +73,7 @@ async function partitionLomuto(ele, l, r){
     await waitforme(delay);
     swap(ele[i], ele[r]); // pivot height one
     console.log(`i = ${i}`, typeof(i));
-    // color
-    ele[r].style.background = 'pink';
-    ele[i].style.background = 'green';
 
-    // pauseChamp
-    await waitforme(delay);
-    
     // color
     for(let k = 0; k < ele.length; k++){
         if(ele[k].style.background != 'green')
@@ -48,16 +85,17 @@ async function partitionLomuto(ele, l, r){
 
 async function quickSort(ele, l, r){
     console.log('In quickSort()', `l=${l} r=${r}`, typeof(l), typeof(r));
-    if(l < r){
-        let pivot_index = await partitionLomuto(ele, l, r);
+    if(l < r) {
+        // let pivot_index = await partitionLomuto(ele, l, r);
+        let pivot_index = await partitionRandom(ele, l, r);
         await quickSort(ele, l, pivot_index - 1);
         await quickSort(ele, pivot_index + 1, r);
     }
-    else{
-        if(l >= 0 && r >= 0 && l <ele.length && r <ele.length){
-            ele[r].style.background = 'green';
+    else if(l >= 0 && r >= 0 && l < ele.length && r < ele.length){
+        // if (ele[l].style.background != 'red')
             ele[l].style.background = 'green';
-        }
+        // if (ele[r].style.background != 'red')
+            ele[r].style.background = 'green';
     }
 }
 
@@ -65,12 +103,10 @@ async function quickSort(ele, l, r){
 const quickSortbtn = document.querySelector(".quickSort");
 quickSortbtn.addEventListener('click', async function(){
     let ele = document.querySelectorAll('.bar');
-    let l = 0;
-    let r = ele.length - 1;
     disableSortingBtn();
     disableSizeSlider();
     disableNewArrayBtn();
-    await quickSort(ele, l, r);
+    await quickSort(ele, 0, ele.length - 1);
     enableSortingBtn();
     enableSizeSlider();
     enableNewArrayBtn();
